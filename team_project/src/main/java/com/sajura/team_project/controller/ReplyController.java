96e61cc422078ce3796b659411483c0588ed5408
LeeSeungKi98 <tmdrlk365w@nate.com>
2023-04-22 02:15:10 +0900

    Team_Project

---
+++ team_project/src/main/java/com/sajura/team_project/controller/ReplyController.java
@@ -0,0 +1,39 @@
+package com.sajura.team_project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sajura.team_project.service.ReplyService;
+import com.sajura.team_project.vo.ReplyVO;
+
+@Controller
+@RequestMapping("/reply")
+public class ReplyController {
+
+	@Autowired
+	ReplyService service;
+	
+	@GetMapping("/insert")
+	public @ResponseBody int insert(ReplyVO vo) {
+		int result=service.insert(vo);
+		return result;
+	}
+	
+	@GetMapping("/delete")
+	public @ResponseBody int delete(int rno){
+		int result=service.delet(rno);
+		System.out.println(result);
+		return result;
+	}
+	
+	@GetMapping("/updat")
+	public int updat(ReplyVO vo) {
+		int result=service.update(vo);
+		return result;
+	}
+	
+}
